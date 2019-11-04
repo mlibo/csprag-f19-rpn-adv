2c1d2aa54802959068e40a742101a4a1eac170ed
Matthew Libo <mlibo@umich.edu>
2019-11-04 12:55:11 -0500

    test support for exp operator

--- test_rpn.py
+++ test_rpn.py
@@ -8,6 +8,9 @@ class TestBasics(unittest.TestCase):
     def test_sub(self):
         result = rpn.calculate("5 3 -")
         self.assertEqual(2, result)
+    def test_exp(self):
+        result = rpn.calculate("5 2 ^")
+        self.assertEqual(25, result)
     def test_badinput(self):
         with self.assertRaises(TypeError):
             rpn.calculate("1 2 3 +")
